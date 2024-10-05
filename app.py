44c0887db48dbe78bc6b270780985328dd168ec2
Akshat Mishra <103744846+Akshatmish@users.noreply.github.com>
2024-10-05 18:05:36 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import pickle
-import sklearn as sn
+import sklearn 
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
